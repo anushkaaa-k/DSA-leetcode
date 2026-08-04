0655aa8a51daead3ac4044c4e6d26d1015236ae2
Anushka <anushkaa7631@gmail.com>
2026-08-05 01:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (52.88%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        mp = {}  
+        
+        for i, num in enumerate(nums):
+            complement = target - num  
+            
+            if complement in mp:       
+                return [mp[complement], i]
+            
+            mp[num] = i             
+            
+        return [-1, -1]  
